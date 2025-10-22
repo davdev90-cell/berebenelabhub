0c56b67067b1f57ec965f9ad2ca967a4dd84ce61
davdev90-cell <davdev90@icloud.com>
2025-10-23 00:36:50 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,6 @@
 
-const CACHE = 'berebenehub-v9-5-1a';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/maskable-512.png',
-  './icons/favicon-32.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  const req = event.request;
-  event.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(resp => {
-      const copy = resp.clone();
-      caches.open(CACHE).then(cache => cache.put(req, copy));
-      return resp;
-    }).catch(() => caches.match('./index.html')))
-  );
-});
+const CACHE = 'berebenehub-v9-5-2';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-512.png','./icons/favicon-32.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { const r=e.request; e.respondWith(caches.match(r).then(c=>c||fetch(r).then(resp=>{const copy=resp.clone(); caches.open(CACHE).then(cc=>cc.put(r,copy)); return resp;}).catch(()=>caches.match('./index.html')))); });
