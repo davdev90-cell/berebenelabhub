5354feba94ad18730f016ca895d6592503711d08
davdev90-cell <davdev90@icloud.com>
2025-10-23 00:42:11 +0100

    Update cache version in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'berebenehub-v9-5-2';
+const CACHE = 'berebenehub-v9-5-2b';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-512.png','./icons/favicon-32.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k))))); self.clients.claim(); });
